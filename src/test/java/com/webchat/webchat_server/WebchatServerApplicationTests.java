9706efc3835a696b60cb353a9e5042e70c24e6e8
windwhisper <1309896340@qq.com>
2024-07-09 23:47:35 +0800

    add regex validation for register service, add jUnit testing for service

--- src/test/java/com/webchat/webchat_server/WebchatServerApplicationTests.java
+++ src/test/java/com/webchat/webchat_server/WebchatServerApplicationTests.java
@@ -1,13 +1,24 @@
 package com.webchat.webchat_server;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
+import com.webchat.webchat_server.service.UserService;
+import com.webchat.webchat_server.service.impl.UserServiceImpl;
+
+// import com.webchat.webchat_server.WebchatServerApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = WebchatServerApplication.class)
 class WebchatServerApplicationTests {
-
+	@Autowired
+	private UserService userService;
 	@Test
-	void contextLoads() {
+	void serviceRegisterRegexTest() {
+		userService.register("sadfasedf12312", "dsfasd3231");
 	}
 
 }
